16bb0825dc6b48e85570953ca91875a30ddb3320
cijb_7724 <johokamia0@gmail.com>
2024-10-28 14:42:19 +0900

    test: deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
-  return(
-      <>
-          <h1>hoge</h1>
-          <p className="text-blue-600 font-bold text-5xl ">
-            Hello Tailwind
-          </p>
-      </>
-  )
+  const [titleColor, setTitleColor] = useState("text-blue-600");
+
+  // 色をトグルする関数
+  const toggleColor = () => {
+    setTitleColor((prevColor) =>
+      prevColor === "text-blue-600" ? "text-red-600" : "text-blue-600"
+    );
+  };
+
+  return (
+    <>
+      <h1>hoge</h1>
+      <p className={`${titleColor} font-bold text-5xl`}>
+        Hello Tailwind
+      </p>
+      <button
+        onClick={toggleColor}
+        className="mt-4 px-4 py-2 bg-gray-600 rounded"
+      >
+        Toggle Color
+      </button>
+    </>
+  );
 }
